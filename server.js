a4f3d527ce5706686009d5cc584dabaa3b758d65
= <=>
2016-07-27 15:51:00 +1200

    two images second

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var express = require('express')
 var app = express ()
 
 app.get ('/' , function (req,res){
-  res.send('<img src = "http://nme.assets.ipccdn.co.uk/images/081022_114642_TheCure03.article_x4.jpg">')
-  res.send('<img src ="http://rocksubculture.com/wp-content/uploads/2016/05/The-Cure-2016-Tour-Concert-Review-Shoreline-Amphitheatre-Live-Photos-FI-1.jpg">')
+  res.send('<img src = "http://nme.assets.ipccdn.co.uk/images/081022_114642_TheCure03.article_x4.jpg">','<img src = "http://rocksubculture.com/wp-content/uploads/2016/05/The-Cure-2016-Tour-Concert-Review-Shoreline-Amphitheatre-Live-Photos-FI-1.jpg">')
 })
 
 var PORT = process.env.PORT || 3000
