dbed707808edc252c783d7856aff7603af645f7d
= <=>
2016-07-27 15:45:29 +1200

    two images

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express ()
 
 app.get ('/' , function (req,res){
   res.send('<img src = "http://nme.assets.ipccdn.co.uk/images/081022_114642_TheCure03.article_x4.jpg">')
+  res.send('<img src ="http://rocksubculture.com/wp-content/uploads/2016/05/The-Cure-2016-Tour-Concert-Review-Shoreline-Amphitheatre-Live-Photos-FI-1.jpg">')
 })
 
 var PORT = process.env.PORT || 3000
